1f06d4313ce09e1e4b4cffdbec4ab60d7afd3b94
Jeremy <jjmccormick@crimson.ua.edu>
2018-10-20 13:08:46 -0500

    Trying using Google Endpoints

---
+++ src/main/java/ClientToken.java
@@ -0,0 +1,16 @@
+public class ClientToken {
+    private String clientToken;
+
+    ClientToken() {
+
+    }
+    ClientToken(String clientToken) {
+        this.clientToken = clientToken;
+    }
+    public String getClientToken() {
+        return this.clientToken;
+    }
+    public void setClientToken(String token) {
+        this.clientToken = clientToken;
+    }
+}
